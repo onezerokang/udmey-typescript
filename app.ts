e6d25b5643a9dd39e88b11aa67fb6766745151c6
Wonyoung Kang <onezerokang@gmail.com>
2022-03-07 22:24:47 +0900

    Type Alias

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
-// 유니언 타입
+type Combinable = number | string;
+type ConversionDescription = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescription
 ) {
   let result;
   if (
@@ -32,3 +34,6 @@ console.log(combinedNames);
 
 // 리터럴 타입은 정확한 값을 가지는 타입이다.
 // 리터럴 타입을 유니언 타입과 조합해서 쓰면 파라미터에 들어와야 하는 정확한 값을 정할 수 있다.
+
+// 타입 알리어스는 복잡한 타입 정의를 원하는 타입이나 타입이름으로 인코딩할 수 있다.
+// 타입 별칭을 사용하면 불필요한 반복을 피하고 타입을 관리할 수 있다.
