5e7971e20acbb01b78747f6231063893d9a01c52
Wonyoung Kang <onezerokang@gmail.com>
2022-03-07 22:17:14 +0900

    Literal type

--- app.ts
+++ app.ts
@@ -1,20 +1,34 @@
 // 유니언 타입
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+  //   if (resultConversion === "as-number") {
+  //     return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Wonyoung", "Kang");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Wonyoung", "Kang", "as-text");
 console.log(combinedNames);
 
-// Union type(조합 타입)을 사용할 때 종종 추가적인 런타임 타입검사를 요구한다.
-// 유니언 타입을 사용하면 매개변수를 보다 유연하게 사용할 수 있다.
-// 다만 타입에 따라 함수의 동작방식이 달라지기 때문에 종종 런타입 검사가 필요하다.
+// 리터럴 타입은 정확한 값을 가지는 타입이다.
+// 리터럴 타입을 유니언 타입과 조합해서 쓰면 파라미터에 들어와야 하는 정확한 값을 정할 수 있다.
